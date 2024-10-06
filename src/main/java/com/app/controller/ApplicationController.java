4dba8cccdea16698cdade8c4c4f3264dfbb12228
SreeSagar09 <sreesagarmamidala09@gmail.com>
2024-10-06 21:51:35 +0530

    try catch blocks are added.

--- src/main/java/com/app/controller/ApplicationController.java
+++ src/main/java/com/app/controller/ApplicationController.java
@@ -17,50 +17,78 @@ public class ApplicationController {
 	
 	@GetMapping(path = "/handlerMethod1")
 	public ResponseEntity<String> handlerMethod1(@RequestParam String param){
-		ResponseEntity<String> responseEntity = new ResponseEntity<String>(param, HttpStatus.OK);
-		
+		ResponseEntity<String> responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<String>(param, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@GetMapping(path = "/handlerMethod2")
 	public ResponseEntity<String[]> handlerMethod2(@RequestParam(name = "param") String[] values){
-		ResponseEntity<String[]>  responseEntity = new ResponseEntity<String[]>(values, HttpStatus.OK);
-		
+		ResponseEntity<String[]>  responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<String[]>(values, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String[]>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@GetMapping(path = "/handlerMethod3")
 	public ResponseEntity<Map<String, String>> handlerMethod3(@RequestParam Map<String, String> params){
-		ResponseEntity<Map<String, String>>  responseEntity = new ResponseEntity<Map<String, String>>(params, HttpStatus.OK);
-		
+		ResponseEntity<Map<String, String>>  responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<Map<String, String>>(params, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<Map<String, String>>(HttpStatus.OK);
+		}
 		return responseEntity;
 	}
 	
 	@GetMapping(path = "/handlerMethod4")
 	public ResponseEntity<MultiValueMap<String, Object>> handlerMethod4(@RequestParam MultiValueMap<String, Object> params){
-		ResponseEntity<MultiValueMap<String, Object>>  responseEntity = new ResponseEntity<MultiValueMap<String, Object>>(params, HttpStatus.OK);
-		
+		ResponseEntity<MultiValueMap<String, Object>>  responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<MultiValueMap<String, Object>>(params, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<MultiValueMap<String, Object>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@GetMapping(path = "/handlerMethod5")
 	public ResponseEntity<List<String>> handlerMethod5(@RequestParam(value = "param") List<String> params){
-		ResponseEntity<List<String>>  responseEntity = new ResponseEntity<List<String>>(params, HttpStatus.OK);
-		
+		ResponseEntity<List<String>>  responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<List<String>>(params, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<List<String>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@GetMapping(path = "/handlerMethod6")
 	public ResponseEntity<String> handlerMethod6(@RequestParam(required = false) String param){
-		ResponseEntity<String>  responseEntity = new ResponseEntity<String>(param, HttpStatus.OK);
-		
+		ResponseEntity<String>  responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<String>(param, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 	
 	@GetMapping(path = "/handlerMethod7")
 	public ResponseEntity<String> handlerMethod7(@RequestParam(defaultValue = "No param value") String param){
-		ResponseEntity<String>  responseEntity = new ResponseEntity<String>(param, HttpStatus.OK);
-		
+		ResponseEntity<String>  responseEntity = null;
+		try {
+			responseEntity = new ResponseEntity<String>(param, HttpStatus.OK);
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return responseEntity;
 	}
 }
